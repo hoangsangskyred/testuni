f64941838b7199023bf24965d5db85ab6ba9ad6b
hoangsangskyred <https://github.com/hoangsangskyred>
2021-03-22 17:42:49 +0700

    demo

--- app/Http/Controllers/Admin/ProjectImageController.php
+++ app/Http/Controllers/Admin/ProjectImageController.php
@@ -26,6 +26,9 @@ class ProjectImageController extends Controller
     public function index(Request $request)
     {
         $this->setRedirectLink($request);
+        
+        $list = ProjectImage::orderBy('created_at','desc')
+        ->paginate(10);
 
         return view($this->view . '.index', ['list' => $this->search($request)])
             ->withController($this);       
@@ -33,7 +36,9 @@ class ProjectImageController extends Controller
 
     public function create()
     {
-        
+        $needle = new ProjectImage();
+
+        return view($this->view.'.create',compact(['needle']))->withController($this);
     }
 
     public function store(Request $request)
